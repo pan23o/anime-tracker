0d128d3511d03cdb0f4092146e8638043e55d5e9
pan23o <157624290+pan23o@users.noreply.github.com>
2026-09-09 20:21:05 +0200

    fix: include TXT restore repair in production build

--- build.mjs
+++ build.mjs
@@ -9,6 +9,7 @@ const files = [
   'profile-enhancer.js',
   'onebase-brand.css',
   'onebase-brand.js',
+  'onebase-persistence-repair.js',
   'favicon.svg'
 ];
 
@@ -26,7 +27,7 @@ let html = await readFile(indexPath, 'utf8');
 html = html.replace(/\s*<link[^>]+rel=["'](?:shortcut )?icon["'][^>]*>/gi, '');
 html = html.replace(/<title>\s*AnimeTracker\s*<\/title>/i, '<title>ONEBASE</title>');
 
-const brandAssets = `\n<link rel="icon" type="image/svg+xml" href="/favicon.svg?v=7">\n<link rel="stylesheet" href="/onebase-brand.css?v=3">\n<script src="/onebase-brand.js?v=3" defer></script>`;
+const brandAssets = `\n<link rel="icon" type="image/svg+xml" href="/favicon.svg?v=8">\n<link rel="stylesheet" href="/onebase-brand.css?v=4">\n<script src="/onebase-brand.js?v=4" defer></script>\n<script src="/onebase-persistence-repair.js?v=1" defer></script>`;
 html = html.replace(/<\/head>/i, `${brandAssets}\n</head>`);
 
 await writeFile(indexPath, html, 'utf8');
